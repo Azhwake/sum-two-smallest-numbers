09d8ef4d13658cfe690f8cac78b20b90ce8aca46
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:33:49 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function sumTwoSmallestNums(arr) {
+  
+    let sortedArr = arr.sort((a, b) => a - b);
+    return sortedArr[0] + sortedArr[1];
+}
+
+
+console.log(sumTwoSmallestNums([12, 5, 24, 55, 18])); 
+console.log(sumTwoSmallestNums([190, 300, 230, 800])); 
